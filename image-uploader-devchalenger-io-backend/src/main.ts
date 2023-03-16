c4e82896d24841996cb4ac6f9aaf533996b3bf66
Luis Eduardo <luiseduess@gmail.com>
2023-03-16 14:01:09 -0300

    feat: add helmet to all routes

--- image-uploader-devchalenger-io-backend/src/main.ts
+++ image-uploader-devchalenger-io-backend/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { initializeFirebase } from './firebase.config';
 
@@ -7,6 +8,7 @@ async function bootstrap() {
   await initializeFirebase();
 
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(3000);
