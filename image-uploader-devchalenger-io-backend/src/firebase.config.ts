637ada81384c5889cd993a2352009d7458b95f3a
Luis Eduardo <luiseduess@gmail.com>
2023-03-16 02:30:36 -0300

    docs: remove readme.md back-end

--- image-uploader-devchalenger-io-backend/src/firebase.config.ts
+++ image-uploader-devchalenger-io-backend/src/firebase.config.ts
@@ -1,6 +1,7 @@
 import * as firebase from 'firebase-admin';
 
 export async function initializeFirebase() {
+  console.log(process.env.FIREBASE);
   const serviceAccount = JSON.parse(process.env.FIREBASE);
   firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccount),
